bc2bf883c66b96b159e2d9d561bbbe4cd54c5884
Yossab_Samuel <yossabpro5@gmail.com>
2025-01-16 10:41:43 +0200

    add memoized fibonacci challenge and the work flow of a chain adding function

--- Kata 5/Memoized Fibonacci.js
+++ Kata 5/Memoized Fibonacci.js
@@ -1,3 +1,5 @@
+// code wars challenge = https://www.codewars.com/kata/529adbf7533b761c560004e5
+// git hub solve : https://github.com/Yossab55/problem-solving-js/blob/main/Kata%205/Memoized%20Fibonacci.js
 function fibonacci(n, memory = {}) {
   console.log(n)
   if(memory[n] != undefined) return memory[n];
