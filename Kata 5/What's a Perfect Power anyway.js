d6a48d0c58777d5581aca20974328a7a5f95b73c
Yossab_Samuel <yossabpro5@gmail.com>
2025-01-16 11:02:29 +0200

    add git hub link

--- Kata 5/What's a Perfect Power anyway.js
+++ Kata 5/What's a Perfect Power anyway.js
@@ -1,5 +1,5 @@
 // link in code wars :https://www.codewars.com/kata/54d4c8b08776e4ad92000835 
-// link in git hub :
+// link in git hub : https://github.com/Yossab55/problem-solving-js/blob/main/Kata%205/What's%20a%20Perfect%20Power%20anyway.js
 var isPP = function (n) {
   let result = null;
   let m = 2;
