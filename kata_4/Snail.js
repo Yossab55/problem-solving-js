113fbac19fd671d9957e2816370187ab0252d267
Yossab_Samuel <yossabpro5@gmail.com>
2025-01-20 07:22:48 +0200

    add answer of other person

--- kata_4/Snail.js
+++ kata_4/Snail.js
@@ -1,4 +1,5 @@
 snail = function (array) {
+  if(array[0][0] == null) return [];
   let result = [];
   let counterItems = array.length ** 2;
   let firstIndex = 0;
@@ -6,7 +7,7 @@ snail = function (array) {
   let directions = ["r", "d", "l", "u"];
   let counterLeftRight = 0;
   result.push(array[firstIndex][secondIndex]);
-  while (counterItems > 1) {
+  while (counterItems != result.length) {
     let direction = directions.shift();
     if (direction == "r") {
       for (
@@ -15,21 +16,18 @@ snail = function (array) {
         secondIndex++
       ) {
         result.push(array[firstIndex][secondIndex]);
-        counterItems--;
       }
       secondIndex--;
     }
     if (direction == "d") {
       for (firstIndex += 1; firstIndex < secondIndex + 1; firstIndex++) {
         result.push(array[firstIndex][secondIndex]);
-        counterItems--;
       }
       firstIndex--;
     }
     if (direction == "l") {
       for (secondIndex -= 1; secondIndex >= counterLeftRight; secondIndex--) {
         result.push(array[firstIndex][secondIndex]);
-        counterItems--;
       }
       secondIndex++;
       counterLeftRight++;
@@ -37,13 +35,11 @@ snail = function (array) {
     if (direction == "u") {
       for (firstIndex -= 1; firstIndex >= secondIndex + 1; firstIndex--) {
         result.push(array[firstIndex][secondIndex]);
-        counterItems--;
       }
       firstIndex++;
     }
     directions.push(direction);
-    console.log(counterItems)
-    // counterItems--;
+    console.log(counterItems);
   }
   return result;
 };
@@ -57,9 +53,32 @@ console.log(
 );
 console.log(
   snail([
-    [1, 2, 3, 10], // 00 01 02 03
-    [4, 5, 6, 11], // 10 11 12 13
-    [7, 8, 9, 12], // 20 21 22 23
-    [0, 0, 0, 13], // 30 31 32 33
+    [1]
   ])
 );
+console.log(
+  snail([
+    [1, 2, 3, 4, 5, 6],
+    [20, 21, 22, 23, 24, 7],
+    [19, 32, 33, 34, 25, 8],
+    [18, 31, 36, 35, 26, 9],
+    [17, 30, 29, 28, 27, 10],
+    [16, 15, 14, 13, 12, 11],
+  ])
+);
+//!more simple code 
+//* he doesn't need first or second index or direction he just pop and shift 
+// snail = function (array) {
+//   var result;
+//   while (array.length) {
+//     // Steal the first row.
+//     result = result ? result.concat(array.shift()) : array.shift();
+//     // Steal the right items.
+//     for (var i = 0; i < array.length; i++) result.push(array[i].pop());
+//     // Steal the bottom row.
+//     result = result.concat((array.pop() || []).reverse());
+//     // Steal the left items.
+//     for (var i = array.length - 1; i >= 0; i--) result.push(array[i].shift());
+//   }
+//   return result;
+// };
