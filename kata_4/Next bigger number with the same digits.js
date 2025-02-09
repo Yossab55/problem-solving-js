17e7a359c387c202003cc4bf1cfeb18ace9b1e48
Yossab_Samuel <yossabpro5@gmail.com>
2025-02-09 07:41:43 +0200

    solve problem kata 4 get next big integer with same didgit

--- kata_4/Next bigger number with the same digits.js
+++ kata_4/Next bigger number with the same digits.js
@@ -1,10 +1,7 @@
 // link in code wars: https://www.codewars.com/kata/55983863da40caa2c900004e
 function nextBigger(n) {
   let result = new NextBigNumber(n);
-  // two errors 
-  // 1- you need to check is there another number could swap note: check test 1
-  // 2- after you make the swap and get the small number take all numbers and make from them
-  // the smallest number
+  
   return result.makeTheResult();
 }
 class NextBigNumber {
@@ -16,50 +13,73 @@ class NextBigNumber {
   }
   makeTheResult() {
     if (this.checkCanSwap() != null) {
+      this.getSmallNumber();
       this.swapToGetBigNumber();
-      if (this.isThereSmallNumber()) {
-        this.swapToGetSmall();
-        return this.reverseTheNumber();
-      }
+      this.changeNumberIndex();
       return this.reverseTheNumber();
     } else return -1;
   }
   checkCanSwap() {
     for (let i = 0; i < this.numberParts.length - 1; i++) {
       if (this.numberParts[i] > this.numberParts[i + 1]) {
-        this.indexToSwap = i;
+        this.numberShouldChange = i + 1;
         return i;
       }
     }
     return null;
   }
-  swapToGetBigNumber() {
-    let temp = this.numberParts[this.indexToSwap];
-    this.numberParts[this.indexToSwap] = this.numberParts[this.indexToSwap + 1];
-    this.numberParts[this.indexToSwap + 1] = temp;
-  }
-  isThereSmallNumber() {
-    for (let i = 0; i < this.indexToSwap; i++) {
-      if (this.numberParts[i] < this.numberParts[this.indexToSwap + 1]) {
+  getSmallNumber() {
+    for (let i = 0; i < this.numberShouldChange; i++) {
+      if (
+        this.numberParts[i] > this.numberParts[this.numberShouldChange] &&
+        this.numberParts[i] < this.numberParts[this.numberShouldChange - 1]
+      ) {
         this.indexToGetSmallNumber = i;
         return true;
       }
     }
-    return false;
+    if (this.indexToGetSmallNumber == undefined)
+      this.indexToGetSmallNumber = this.numberShouldChange - 1;
   }
-  swapToGetSmall() {
-    let temp = this.numberParts[this.indexToSwap ];
-    this.numberParts[this.indexToSwap] =
+  swapToGetBigNumber() {
+    let temp = this.numberParts[this.numberShouldChange];
+    this.numberParts[this.numberShouldChange] =
       this.numberParts[this.indexToGetSmallNumber];
     this.numberParts[this.indexToGetSmallNumber] = temp;
   }
+  changeNumberIndex() {
+    let arr = this.sortTheNumberAbove(this.getNumberAbove());
+    for(let i = 0; i <arr.length; i++) {
+      this.numberParts[i] = arr[i];
+    }
+  }
+  sortTheNumberAbove(arr) {
+    if(arr.length == 1 || arr.length == 0) return arr;
+    let middle = parseInt(arr.length / 2);
+    let big = [];
+    let small = [];
+    for(let i = 0; i < arr.length; i++) {
+      if(i == middle) continue;
+      if(arr[i] >= arr[middle]) big.push(arr[i]);
+      if(arr[i] < arr[middle]) small.push(arr[i]);
+    }
+    return this.sortTheNumberAbove(big).concat(arr[middle]).concat(this.sortTheNumberAbove(small));
+  }
+  getNumberAbove() {
+    let result = [];
+    for(let i = 0; i < this.numberShouldChange; i++) {
+      result.push(this.numberParts[i]);
+    }
+    return result;
+  }
+
   reverseTheNumber() {
-    return Number(this.numberParts.reverse().join(''));
+    return Number(this.numberParts.reverse().join(""));
   }
 }
-// console.log(nextBigger(315)); 
-// console.log(nextBigger(12)); 
+// console.log(nextBigger(315));
+// console.log(nextBigger(12));
 console.log(nextBigger(791)); // 917    197    179  719
-console.log(nextBigger(24325358108882)); 
+console.log(nextBigger(24325358108882));
 console.log(nextBigger(39090678410)); //39090680147
-// console.log(nextBigger(111)); 
+// console.log(nextBigger(111));
